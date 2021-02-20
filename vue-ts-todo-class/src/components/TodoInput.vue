bd7ed390178f030c627bf51c3a0023adf732d7f8
kureharyosuke <ohhayo.com@gmail.com>
2021-02-21 02:17:25 +0900

    vue ts Class

--- vue-ts-todo-class/src/components/TodoInput.vue
+++ vue-ts-todo-class/src/components/TodoInput.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <label for="todo-input"></label>
-    <input id="todo-input" type="text" />
+    <input id="todo-input" type="text" :value="todoText" />
     <button @click="onClickInput" type="button"></button>
   </div>
 </template>
@@ -11,6 +11,9 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 
 @Component
 export default class extends Vue {
+  @Prop({ default: "" })
+  item!: string;
+
   private onClinkInput(): void {
     console.log("click input");
   }
